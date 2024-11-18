d89b1993b81e9ab593660451f098e57473485c42
TedMoon99 <mtj1015@naver.com>
2024-11-18 14:33:30 +0900

    Component 패키지 추가

--- app/src/main/java/com/example/toucheeseapp/presentation/ui/feature_screen/StudioListScreen.kt
+++ app/src/main/java/com/example/toucheeseapp/presentation/ui/feature_screen/StudioListScreen.kt
@@ -1,8 +1,19 @@
 package com.example.toucheeseapp.presentation.ui.feature_screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.toucheeseapp.presentation.ui.theme.ToucheeseAppTheme
 
 @Composable
 fun StudioListScreen() {
 
+
+}
+
+@Preview
+@Composable
+fun StudioListPreview() {
+    ToucheeseAppTheme{
+        StudioListScreen()
+    }
 }
