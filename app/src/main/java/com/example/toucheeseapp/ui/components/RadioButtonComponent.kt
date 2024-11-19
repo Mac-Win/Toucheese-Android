879caa558631f4821f52f469f6dffd802c475d1f
TedMoon99 <mtj1015@naver.com>
2024-11-20 02:14:43 +0900

    [UI] 필터링 RadioButtonComponent.kt 생성

---
+++ app/src/main/java/com/example/toucheeseapp/ui/components/RadioButtonComponent.kt
@@ -0,0 +1,43 @@
+package com.example.toucheeseapp.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonColors
+import androidx.compose.material3.RadioButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun RadioButtonComponent(selected: Boolean, filterItemName: String, modifier: Modifier = Modifier, onClick: () -> Unit,) {
+
+    Column(
+        modifier= modifier.padding(4.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = filterItemName,
+            fontSize = 16.sp
+
+        )
+
+        RadioButton(
+            selected= selected,
+            onClick = onClick,
+            colors = RadioButtonDefaults.colors(
+                selectedColor = Color(0xFFFFC000),
+                unselectedColor = Color(0xFFFFE085)
+            )
+
+        )
+
+
+    }
+}
