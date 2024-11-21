fc7ef8cd175fd1127e296cc2d47fcadc6cda596f
TedMoon99 <mtj1015@naver.com>
2024-11-21 18:47:09 +0900

    [Feat] Studio Data StudioListScreen.kt 연결

--- app/src/main/java/com/example/toucheeseapp/ui/screens/StudioListScreen.kt
+++ app/src/main/java/com/example/toucheeseapp/ui/screens/StudioListScreen.kt
@@ -1,14 +1,17 @@
 package com.example.toucheeseapp.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -16,16 +19,20 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.toucheeseapp.ui.components.FilterChipComponent
 import com.example.toucheeseapp.ui.components.StudioListItemComponent
 import com.example.toucheeseapp.ui.components.TopAppBarComponent
 import com.example.toucheeseapp.ui.theme.ToucheeseAppTheme
+import com.example.toucheeseapp.ui.viewmodel.StudioViewModel
 
 @Composable
-fun StudioListScreen(onClickLeadingIcon: () -> Unit, onClickTrailingIcon: () -> Unit) {
+fun StudioListScreen(viewModel: StudioViewModel = hiltViewModel(), onClickLeadingIcon: () -> Unit, onClickTrailingIcon: () -> Unit) {
     var selectedIndex by remember { mutableStateOf(-1) }
     var selectedFilter by remember { mutableStateOf("") }
+    val studios by viewModel.studios.collectAsState()
+    Log.d("StudioListScreen", "불러온 스튜디오 리스트 조회 ${studios}")
     Scaffold(
         topBar = {
             TopAppBarComponent(
@@ -49,13 +56,8 @@ fun StudioListScreen(onClickLeadingIcon: () -> Unit, onClickTrailingIcon: () -> 
 
             // 스튜디오 리스트
             LazyColumn() {
-                item {
-                    StudioListItemComponent(true)
-                    StudioListItemComponent(true)
-                    StudioListItemComponent(true)
-                    StudioListItemComponent(true)
-                    StudioListItemComponent(true)
-                    StudioListItemComponent(true)
+                items(studios){
+                    StudioListItemComponent(it, true)
                 }
             }
 
