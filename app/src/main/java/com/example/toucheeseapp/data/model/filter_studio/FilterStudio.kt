d47b7aaaa5b47ea5153613c5e415ef2fd2b5eeea
TedMoon99 <mtj1015@naver.com>
2024-11-22 11:58:02 +0900

    [DTO] 필터링된 스튜디오 리스트 - JSON 맵핑 데이터 생성

---
+++ app/src/main/java/com/example/toucheeseapp/data/model/filter_studio/FilterStudio.kt
@@ -0,0 +1,17 @@
+package com.example.toucheeseapp.data.model.filter_studio
+
+
+import com.google.gson.annotations.SerializedName
+
+data class FilterStudio(
+    @SerializedName("imageUrls")
+    val images: List<String>,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("price")
+    val price: Int,
+    @SerializedName("profileImage")
+    val profileImage: String,
+    @SerializedName("rating")
+    val rating: Double
+)
