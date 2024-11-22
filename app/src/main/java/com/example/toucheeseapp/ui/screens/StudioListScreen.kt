cd465f89d44852314987f90294f2f2c473bb7ba6
TedMoon99 <mtj1015@naver.com>
2024-11-22 11:56:31 +0900

    [UI] Filter Chip 다중 선택으로 변경

--- app/src/main/java/com/example/toucheeseapp/ui/screens/StudioListScreen.kt
+++ app/src/main/java/com/example/toucheeseapp/ui/screens/StudioListScreen.kt
@@ -6,10 +6,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -17,22 +14,19 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
 import com.example.toucheeseapp.ui.components.FilterChipComponent
 import com.example.toucheeseapp.ui.components.StudioListItemComponent
 import com.example.toucheeseapp.ui.components.TopAppBarComponent
-import com.example.toucheeseapp.ui.theme.ToucheeseAppTheme
 import com.example.toucheeseapp.ui.viewmodel.StudioViewModel
 
 @Composable
 fun StudioListScreen(viewModel: StudioViewModel = hiltViewModel(), onClickLeadingIcon: () -> Unit, onClickTrailingIcon: () -> Unit) {
-    var selectedIndex by remember { mutableStateOf(-1) }
-    var selectedFilter by remember { mutableStateOf("") }
+    var selectedIndex by remember { mutableStateOf(-1) } // 선택된 chip Index
+    var selectedFilters by remember { mutableStateOf(mapOf<Int, Int>()) } // 다중 선택을 위한 Set
     val studios by viewModel.studios.collectAsState()
-    Log.d("StudioListScreen", "불러온 스튜디오 리스트 조회 ${studios}")
+
     Scaffold(
         topBar = {
             TopAppBarComponent(
@@ -48,27 +42,28 @@ fun StudioListScreen(viewModel: StudioViewModel = hiltViewModel(), onClickLeadin
             // 필터
             FilterChipComponent(
                 selectedIndex= selectedIndex,
-                selectedFilter = selectedFilter,
-                onSelectedItemChange = { selectedIndex = if (selectedIndex == it) -1 else it },
-                onSelectedFilterChange = {selectedFilter = it },
+                selectedFilters = selectedFilters,
+                onSelectedItemChange = { selectedIndex = if(selectedIndex == it) -1 else it},
+                onSelectedFilterChange = { chipIndex, selectedFilterIndex ->
+                    selectedFilters = selectedFilters.toMutableMap().apply {
+                        this[chipIndex] = selectedFilterIndex
+                    }
+                    // 선택 후 자동으로 필터 닫힘
+                    selectedIndex = -1
+                    // 필터링 로직 호출
+
+                    viewModel.filterStudio(3, selectedFilters)
+                    Log.d("Filter", "selectedFilters = ${selectedFilters}")
+                },
                 modifier = Modifier.padding(16.dp)
             )
 
             // 스튜디오 리스트
             LazyColumn() {
-                items(studios){
-                    StudioListItemComponent(it, true)
-                }
+                items(studios){ StudioListItemComponent(it, true) }
             }
 
         }
     }
 
-}
-
-@Preview
-@Composable
-fun StudioListPreview() {
-    ToucheeseAppTheme{
-    }
 }
