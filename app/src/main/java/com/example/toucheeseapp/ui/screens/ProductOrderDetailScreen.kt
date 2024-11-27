73e21b24106b7c089e83d45473987e4576ca7109
TedMoon99 <mtj1015@naver.com>
2024-11-27 15:10:52 +0900

    [UI] 주문상품상세조회 UI 파일 생성

---
+++ app/src/main/java/com/example/toucheeseapp/ui/screens/ProductOrderDetailScreen.kt
@@ -0,0 +1,16 @@
+package com.example.toucheeseapp.ui.screens
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.toucheeseapp.ui.viewmodel.StudioViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProductOrderDetailScreen(
+    viewModel: StudioViewModel = hiltViewModel(),
+    modifier: Modifier = Modifier
+) {
+
+}
