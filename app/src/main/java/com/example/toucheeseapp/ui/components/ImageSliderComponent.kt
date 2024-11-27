0a05b971746d7a4739c9fcfb268ebbf399e1f8e5
songjuseok <realsongzi93@gmail.com>
2024-11-27 15:06:27 +0900

    [Gradle] StudioDetailScreen.kt 화면에 ImageSliderComponent 추가

---
+++ app/src/main/java/com/example/toucheeseapp/ui/components/ImageSliderComponent.kt
@@ -0,0 +1,30 @@
+package com.example.toucheeseapp.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil3.compose.rememberAsyncImagePainter
+
+@Composable
+fun ImageSliderComponent(images: List<String>, modifier: Modifier = Modifier) {
+    val pagerState = rememberPagerState(initialPage = 0, pageCount = { images.size })
+
+    HorizontalPager(
+        state = pagerState,
+        modifier = modifier
+            .fillMaxWidth()
+            .height(200.dp)
+    ) { page ->
+        Image(
+            painter = rememberAsyncImagePainter(model = images[page]),
+            contentDescription = "스튜디오 이미지",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
