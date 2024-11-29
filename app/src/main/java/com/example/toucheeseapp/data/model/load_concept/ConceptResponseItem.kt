15a4808df1622b7990cc04c40d97239b4f05a42f
TedMoon99 <mtj1015@naver.com>
2024-11-29 14:43:24 +0900

    [Feat] 컨셉 조회 DTO

---
+++ app/src/main/java/com/example/toucheeseapp/data/model/load_concept/ConceptResponseItem.kt
@@ -0,0 +1,11 @@
+package com.example.toucheeseapp.data.model.load_concept
+
+
+import com.google.gson.annotations.SerializedName
+
+data class ConceptResponseItem(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String
+)
