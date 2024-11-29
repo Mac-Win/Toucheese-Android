581a35dc907e149d2fe64bc1b2a622999f036786
TedMoon99 <mtj1015@naver.com>
2024-11-30 00:02:38 +0900

    [Style] 불필요한 코드 제거

--- app/src/main/java/com/example/toucheeseapp/ui/components/ReviewItemComponent.kt
+++ app/src/main/java/com/example/toucheeseapp/ui/components/ReviewItemComponent.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import com.example.toucheeseapp.data.model.review_studio.StudioReviewResponseItem
@@ -21,24 +22,8 @@ fun ReviewItemComponent(review: StudioReviewResponseItem, modifier: Modifier = M
         AsyncImage(
             model = review.firstImage,
             contentDescription = "Review Image",
+            contentScale = ContentScale.Crop,
             modifier = Modifier.fillMaxSize()
         )
     }
 }
-
-// 임시 데이터 모델
-data class Review(val imageUrl: String, val id: Int)
-
-// 임시 데이터 리스트
-val dummyReviews = listOf(
-    Review(id = 1, imageUrl = "https://via.placeholder.com/300x200"),
-    Review(imageUrl = "https://via.placeholder.com/300x200", id = 2),
-    Review(imageUrl = "https://via.placeholder.com/300x200", id = 3),
-    Review(imageUrl = "https://via.placeholder.com/300x200", id = 4),
-    Review(imageUrl = "https://via.placeholder.com/300x200", id = 5),
-    Review(imageUrl = "https://via.placeholder.com/300x200", id = 6),
-    Review(imageUrl = "https://via.placeholder.com/300x200", id = 7),
-    Review(imageUrl = "https://via.placeholder.com/300x200", id = 8),
-    Review(imageUrl = "https://via.placeholder.com/300x200", id = 9),
-    Review(imageUrl = "https://via.placeholder.com/300x200", id = 10)
-)
