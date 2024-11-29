4ad40753a28c605d16e3716602cac70e646f9774
songjuseok <realsongzi93@gmail.com>
2024-11-29 15:56:10 +0900

    [Fix] ToucheeseServer.kt 서버 API 수정

--- app/src/main/java/com/example/toucheeseapp/data/network/ToucheeseServer.kt
+++ app/src/main/java/com/example/toucheeseapp/data/network/ToucheeseServer.kt
@@ -3,23 +3,20 @@ package com.example.toucheeseapp.data.network
 import com.example.toucheeseapp.data.model.concept_studio.StudioResponse
 import com.example.toucheeseapp.data.model.filter_studio.FilterResponse
 import com.example.toucheeseapp.data.model.search_studio.SearchResponse
-import okhttp3.Response
-import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.HTTP
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ToucheeseServer {
     // 스튜디오 컨셉별 데이터 조회
-    @GET("v1/concepts/{id}/studios") // baseUrl 뒤 상대경로
+    @GET("v1/concepts/{conceptid}/studios") // baseUrl 뒤 상대경로
     suspend fun getStudios(
         @Path("id") conceptId: Int, // 동적 경로 파라미터
         @Query("page") page: Int // 쿼리 파라미터
     ): StudioResponse
 
     // 검색 스튜디오 데이터 조회
-    @GET("v1/studios/search")
+    @GET("v1/studios")
     suspend fun searchStudios(
         @Query("keyword") keyword: String
     ): SearchResponse
@@ -33,6 +30,5 @@ interface ToucheeseServer {
         @Query("rating") rating: Double?,
         @Query("locations") locations: List<String>?,
     ): FilterResponse
-
 
 }
