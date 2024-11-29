d0df6a6fdf3b4a5b1d9fb1e973bfdc7d2194f923
songjuseok <realsongzi93@gmail.com>
2024-11-29 15:54:31 +0900

    [Feat] ReviewStudioInfoAndCommentComponent.kt 리뷰 댓글 창에 리뷰스튜디오정보와 댓글내용

---
+++ app/src/main/java/com/example/toucheeseapp/ui/components/ReviewStudioInfoAndCommentComponent.kt
@@ -0,0 +1,88 @@
+package com.example.toucheeseapp.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil3.compose.rememberAsyncImagePainter
+import com.example.toucheeseapp.data.model.concept_studio.Studio
+
+@Composable
+fun ReviewStudioAndCommentComponent(
+    studio: Studio?,
+    comments: List<String>
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp) // 내부 패딩
+    ) {
+        // 스튜디오 프로필
+        if (studio != null) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                // 스튜디오 프로필 이미지
+                Image(
+                    painter = rememberAsyncImagePainter(model = studio.profileImage),
+                    contentDescription = "스튜디오 프로필 이미지",
+                    modifier = Modifier
+                        .size(60.dp)
+                        .background(Color.Gray, CircleShape),
+                    contentScale = ContentScale.Crop
+                )
+
+                Spacer(modifier = Modifier.width(16.dp))
+
+                // 스튜디오 이름
+                Text(
+                    text = studio.name,
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // 댓글 리스트
+        comments.forEach { comments ->
+
+            // 댓글 텍스트
+            Text(
+                text = comments,
+                modifier = Modifier.padding(vertical = 4.dp),
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.Black
+            )
+
+            // 댓글 사이의 구분선
+            Divider(
+                color = Color.Gray.copy(alpha = 0.5f),
+                thickness = 1.dp,
+                modifier = Modifier.padding(vertical = 4.dp)
+            )
+        }
+    }
+}
+
+val dummyComments = listOf(
+    "고객님 감사합니다. 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구" +
+            "어쩌구 저쩌구 어쩌구 저쩌구어쩌구 저쩌구 어쩌구 저쩌구어쩌구 저쩌구 어쩌구 저쩌구" +
+            "어쩌구 저쩌구 어쩌구 저쩌구어쩌구 저쩌구 어쩌구 저쩌구어쩌구 저쩌구 어쩌구 저쩌구" +
+            "어쩌구 저쩌구 어쩌구 저쩌구어쩌구 저쩌구 어쩌구 저쩌구어쩌구 저쩌구 어쩌구 저쩌구" +
+            "어쩌구 저쩌구 어쩌구 저쩌구어쩌구 저쩌구 어쩌구 저쩌구어쩌구 저쩌구 어쩌구 저쩌구" +
+            "어쩌구 저쩌구 어쩌구 저쩌구어쩌구 저쩌구 어쩌구 저쩌구어쩌구 저쩌구 어쩌구 저쩌구"
+)
