fbf5f424fe3842b18744a2acde9f5c46202d7683
TedMoon99 <mtj1015@naver.com>
2024-11-29 14:44:07 +0900

    [Feat] 스튜디오 검색, 스튜디오 상세 조회, 컨셉 조회 API

--- app/src/main/java/com/example/toucheeseapp/data/network/ToucheeseServer.kt
+++ app/src/main/java/com/example/toucheeseapp/data/network/ToucheeseServer.kt
@@ -3,35 +3,34 @@ package com.example.toucheeseapp.data.network
 import com.example.toucheeseapp.data.model.specific_review.ReviewResponse
 import com.example.toucheeseapp.data.model.concept_studio.StudioResponse
 import com.example.toucheeseapp.data.model.filter_studio.FilterResponse
+import com.example.toucheeseapp.data.model.load_concept.ConceptResponse
 import com.example.toucheeseapp.data.model.review_studio.StudioReviewResponse
 import com.example.toucheeseapp.data.model.search_studio.SearchResponse
+import com.example.toucheeseapp.data.model.studio_detail.StudioDetailResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ToucheeseServer {
-    // 스튜디오 컨셉별 데이터 조회
-    @GET("v1/concepts/{id}/studios") // baseUrl 뒤 상대경로
+    // -------- 해당 컨셉 스튜디오 API --------
+    // 컨셉 스튜디오 목록 조회
+    @GET("v1/concepts/{conceptId}/studios") // baseUrl 뒤 상대경로
     suspend fun getStudios(
-        @Path("id") conceptId: Int, // 동적 경로 파라미터
+        @Path("conceptId") conceptId: Int, // 동적 경로 파라미터
         @Query("page") page: Int // 쿼리 파라미터
     ): StudioResponse
 
-    // 검색 스튜디오 데이터 조회
-    @GET("v1/studios/search")
-    suspend fun searchStudios(
-        @Query("keyword") keyword: String
-    ): SearchResponse
-
-    // 필터 스튜디오 데이터 조회
-    @GET("v1/studios/{id}/filters")
+    // 필터 적용 후 스튜디오 목록 조회
+    @GET("v1/concepts/{conceptId}/filters")
     suspend fun filterStudio(
-        @Path("id") conceptId: Int,
+        @Path("conceptId") conceptId: Int,
         @Query("page") page: Int,
         @Query("price") price: Int?,
         @Query("rating") rating: Double?,
         @Query("locations") locations: List<String>?,
     ): FilterResponse
+
+    // -------- 리뷰 API --------
 
     // 스튜디오 리뷰 목록 조회
     @GET("v1/studios/{studioId}/reviews")
@@ -52,4 +51,26 @@ interface ToucheeseServer {
         @Path("studioId") studioId: Int,
         @Path("productId") productId: Int,
     )
+
+    // -------- 스튜디오 API --------
+
+    // 스튜디오 검색
+    @GET("v1/studios")
+    suspend fun searchStudios(
+        @Query("keyword") keyword: String
+    ): SearchResponse
+
+    // 스튜디오 상세 조회
+    @GET("v1/studios/{studioId}")
+    suspend fun loadStudioDetail(
+        @Path("studioId") studioId: Int
+    ): StudioDetailResponse
+
+    // -------- 컨셉 API --------
+
+    // 컨셉 조회
+    @GET("v1/concepts")
+    suspend fun loadConcept(): ConceptResponse
+
+
 }
