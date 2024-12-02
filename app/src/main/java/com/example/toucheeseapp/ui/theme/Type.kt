e79ef3fd849e16070a14c3d5557182ab1fbe403c
TedMoon99 <mtj1015@naver.com>
2024-12-03 00:46:29 +0900

    [ui] 서체(font) 추가

--- app/src/main/java/com/example/toucheeseapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/toucheeseapp/ui/theme/Type.kt
@@ -2,33 +2,19 @@ package com.example.toucheeseapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.toucheeseapp.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+val NotoSansKr = FontFamily(
+    Font(R.font.noto_sans_kr_black),
+    Font(R.font.noto_sans_kr_bold),
+    Font(R.font.noto_sans_kr_extra_bold),
+    Font(R.font.noto_sans_kr_extra_light),
+    Font(R.font.noto_sans_kr_medium),
+    Font(R.font.noto_sans_kr_regular),
+    Font(R.font.noto_sans_kr_semi_bold),
+    Font(R.font.noto_sans_kr_thin)
 )
