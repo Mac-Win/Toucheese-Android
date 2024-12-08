7ca53c37b91b4eac67a00663b7c83e9ca0ba49fc
TedMoon99 <mtj1015@naver.com>
2024-12-09 01:23:39 +0900

    [UI] 스튜디오 상세 조회 UI <운영시간>수정

--- app/src/main/java/com/example/toucheeseapp/ui/screens/tab_Home/StudioDetailScreen.kt
+++ app/src/main/java/com/example/toucheeseapp/ui/screens/tab_Home/StudioDetailScreen.kt
@@ -1,6 +1,7 @@
 package com.example.toucheeseapp.ui.screens.tab_Home
 
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
@@ -13,6 +14,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,6 +26,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.toucheeseapp.ui.components.BottomActionButtons
@@ -101,7 +105,13 @@ fun StudioDetailScreen(
 
                 // 스튜디오 정보
                 item {
-                    StudioInfoComponent(studio = studio!!)
+                    StudioInfoComponent(
+                        studio = studio!!,
+                        modifier = Modifier
+                        .fillMaxWidth() // 화면 너비를 채움
+                        .background(Color(0xFFFFF2CC)) // 노란 배경
+                        .clip(RoundedCornerShape(8.dp)) // 모서리 둥글게
+                   )
                 }
 
                 // 탭 컴포넌트
