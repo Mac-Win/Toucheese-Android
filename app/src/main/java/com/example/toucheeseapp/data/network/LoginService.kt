a7fd3c32caa941dda4bf3c703cf358851b0f562a
TedMoon99 <mtj1015@naver.com>
2024-12-09 15:33:30 +0900

    [Fix] 로그인 API 수정

--- app/src/main/java/com/example/toucheeseapp/data/network/LoginService.kt
+++ app/src/main/java/com/example/toucheeseapp/data/network/LoginService.kt
@@ -1,6 +1,7 @@
 package com.example.toucheeseapp.data.network
 
 import com.example.toucheeseapp.data.model.login.Login
+import com.example.toucheeseapp.data.model.login.LoginResponseBody
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -10,6 +11,6 @@ interface LoginService {
 
     // 회원 로그인
     @POST("v1/members")
-    suspend fun requestLogin(@Body login: Login): Response<Unit> // 본문이 없으므로 Unit 사용
+    suspend fun requestLogin(@Body login: Login): Response<LoginResponseBody>
 
 }
