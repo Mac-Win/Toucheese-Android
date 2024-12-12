e84be97e80420fa68e46ceb1abfd0313265306e8
TedMoon99 <mtj1015@naver.com>
2024-12-12 14:12:34 +0900

    [Fix] 결제 조회 기능 API 맵핑 이름 수정

--- app/src/main/java/com/example/toucheeseapp/data/model/cart_order_pay/OrderPayResponse.kt
+++ app/src/main/java/com/example/toucheeseapp/data/model/cart_order_pay/OrderPayResponse.kt
@@ -4,7 +4,7 @@ package com.example.toucheeseapp.data.model.cart_order_pay
 import com.google.gson.annotations.SerializedName
 
 data class OrderPayResponse(
-    @SerializedName("cartPaymentList")
+    @SerializedName("CheckoutCartItems")
     val cartPaymentList: List<CartPayment>,
     @SerializedName("memberContactInfo")
     val memberContactInfo: MemberContactInfo
