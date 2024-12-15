e5a71db21a86f7a96c52bb69daaddb43c82f003b
TedMoon99 <mtj1015@naver.com>
2024-12-15 21:48:08 +0900

    [UI] [문의하기 화면] 답변 상태에 따라 위치 배정

--- app/src/main/java/com/example/toucheeseapp/ui/screens/tab_Qna/QnaScreen.kt
+++ app/src/main/java/com/example/toucheeseapp/ui/screens/tab_Qna/QnaScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -70,9 +71,18 @@ fun QnaScreen(
             verticalArrangement = Arrangement.spacedBy(10.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
+            // 답변 완료 리스트 - 추후 로직 수정
+            val didReplyList = testDataList.filter { item ->
+                item.isNotEmpty()
+            }
 
-            // 문의 내역 리스트
-            items(testDataList) { item ->
+            // 답변 대기 리스트 - 추후 로직 수정
+            val didNotReplyList = testDataList.filter { item ->
+                item.isEmpty()
+            }
+
+            // 문의 내역 리스트 - 답변 대기
+            items(didNotReplyList) { item ->
                 // 문의 내역 아이템
                 InfoListItemComponent(
                     title = "Q.${item}",
@@ -104,6 +114,28 @@ fun QnaScreen(
                         onButtonClicked = onButtonClicked,
                     )
                 }
+            }
+            item {
+                HorizontalDivider(modifier = Modifier.fillMaxWidth())
+            }
+
+            items(didReplyList) { item ->
+                // 문의 내역 아이템
+                InfoListItemComponent(
+                    title = "Q.${item}",
+                    content = "",
+                    createDate = "2024-12-15",
+                    userName = "홍길동",
+                    replyState = item.isNotEmpty(),
+                    isContentShowed = false,
+                    modifier = Modifier.padding(
+                        start = 12.dp,
+                        end = 12.dp,
+                        bottom = 12.dp,
+                        top = 16.dp
+                    ),
+                    onItemClicked = onItemClicked,
+                )
             }
 
         }
