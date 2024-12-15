a961c59b99c55decb1b201f75f8a97de330fdb6b
TedMoon99 <mtj1015@naver.com>
2024-12-16 03:19:19 +0900

    [Style] [QnaViewModel] 임시 hilt 비활성화

--- app/src/main/java/com/example/toucheeseapp/ui/viewmodel/QnaViewModel.kt
+++ app/src/main/java/com/example/toucheeseapp/ui/viewmodel/QnaViewModel.kt
@@ -5,9 +5,9 @@ import com.example.toucheeseapp.data.repository.QnaRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
-@HiltViewModel
-class QnaViewModel @Inject constructor(
+//@HiltViewModel
+class QnaViewModel /*@Inject constructor(
     private val repository: QnaRepository
 ): ViewModel() {
 
-}
+}*/
