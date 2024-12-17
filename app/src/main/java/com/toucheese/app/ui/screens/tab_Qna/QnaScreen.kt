4696d3129cf5994f03e29aa81778eb35f4f64ee4
TedMoon99 <mtj1015@naver.com>
2024-12-17 19:42:36 +0900

    [UI] 문의 작성하기 버튼 -> 문의 작성하기 FloatingActionButton으로 변경

--- app/src/main/java/com/toucheese/app/ui/screens/tab_Qna/QnaScreen.kt
+++ app/src/main/java/com/toucheese/app/ui/screens/tab_Qna/QnaScreen.kt
@@ -2,23 +2,29 @@ package com.toucheese.app.ui.screens.tab_Qna
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.toucheese.app.ui.components.BottomNavigationBarComponent
 import com.toucheese.app.ui.components.listitem.InfoListItemComponent
-import com.toucheese.app.ui.components.textbutton.IconButtonComponent
 import com.toucheese.app.ui.components.topbar.TopAppBarComponent
 import com.toucheese.app.ui.viewmodel.QnaViewModel
 
@@ -31,6 +37,21 @@ fun QnaScreen(
     onItemClicked: () -> Unit,
     onButtonClicked: () -> Unit,
 ) {
+    // LazyColumn의 스크롤 상태 추적
+    val listState = rememberLazyListState()
+
+    // FAB의 가시성 상태
+    val isFabVisible by remember (listState) {
+        derivedStateOf {
+            val visibleItemInfo = listState.layoutInfo.visibleItemsInfo
+            if (visibleItemInfo.isEmpty()) true
+            else {
+                val lastVisibleItemIndex = visibleItemInfo.lastOrNull()?.index ?: 0
+                lastVisibleItemIndex < (listState.layoutInfo.totalItemsCount - 1)
+            }
+        }
+    }
+
     Scaffold(
         topBar = {
             TopAppBarComponent(
@@ -45,6 +66,22 @@ fun QnaScreen(
                 selectedTab = selectedTab,
                 onTabSelected = onTabSelected,
             )
+        },
+
+        floatingActionButton = {
+            if (isFabVisible){
+                FloatingActionButton(
+                    shape = RoundedCornerShape(8.dp),
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    elevation = FloatingActionButtonDefaults.elevation(4.dp),
+                    onClick = onButtonClicked,
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = null,
+                    )
+                }
+            }
         }
     ) { innerPadding ->
         // 임시 데이터 -> 추후 데이터 연결 때 객체 리스트로 전환
@@ -65,26 +102,18 @@ fun QnaScreen(
             "테스트 문의4"
         )
         LazyColumn(
+            state = listState,
             modifier = Modifier.padding(innerPadding),
             contentPadding = PaddingValues(16.dp),
             verticalArrangement = Arrangement.spacedBy(10.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            // 답변 완료 리스트 - 추후 로직 수정
-            val didReplyList = testDataList.filter { item ->
-                item.isNotEmpty()
-            }
-
-            // 답변 대기 리스트 - 추후 로직 수정
-            val didNotReplyList = testDataList.filter { item ->
-                item.isEmpty()
-            }
 
             // 문의 내역 리스트 - 답변 대기
-            items(didNotReplyList) { item ->
+            items(testDataList) { item ->
                 // 문의 내역 아이템
                 InfoListItemComponent(
-                    title = "Q.${item}",
+                    title = item,
                     content = "",
                     createDate = "2024-12-15",
                     userName = "홍길동",
@@ -99,44 +128,6 @@ fun QnaScreen(
                     onItemClicked = onItemClicked,
                 )
             }
-
-            // 문의 작성하기 버튼
-            item {
-                Row(
-                    modifier = Modifier.padding(vertical = 24.dp),
-                ) {
-                    IconButtonComponent(
-                        icon = Icons.Default.Add,
-                        label = "문의 작성하기",
-                        enabled = true,
-                        modifier = Modifier.fillMaxWidth(),
-                        onButtonClicked = onButtonClicked,
-                    )
-                }
-            }
-            item {
-                HorizontalDivider(modifier = Modifier.fillMaxWidth())
-            }
-
-            items(didReplyList) { item ->
-                // 문의 내역 아이템
-                InfoListItemComponent(
-                    title = "Q.${item}",
-                    content = "",
-                    createDate = "2024-12-15",
-                    userName = "홍길동",
-                    replyState = item.isNotEmpty(),
-                    isContentShowed = false,
-                    modifier = Modifier.padding(
-                        start = 12.dp,
-                        end = 12.dp,
-                        bottom = 12.dp,
-                        top = 16.dp
-                    ),
-                    onItemClicked = onItemClicked,
-                )
-            }
-
         }
 
     }
