21388f95045431f64a5b783999f067b0dd29935c
songjuseok <realsongzi93@gmail.com>
2024-12-18 00:45:03 +0900

    [UI] BookingScheduleItemComponent.kt 예약 일정 페이지에 들어가는 카드

--- app/src/main/java/com/example/toucheeseapp/ui/components/BookingScheduleItemComponent.kt
+++ app/src/main/java/com/example/toucheeseapp/ui/components/BookingScheduleItemComponent.kt
@@ -1,21 +1,30 @@
 package com.example.toucheeseapp.ui.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SuggestionChip
+import androidx.compose.material3.SuggestionChipDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,59 +37,119 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.toucheeseapp.R
+import com.example.toucheeseapp.ui.theme.ToucheeseAppTheme
 
 @Composable
-fun bookingScheduleItemComponent(modifier: Modifier = Modifier){
+fun BookingScheduleItemComponent(
+    modifier: Modifier = Modifier,
+    showButton: Boolean = true,
+    ){
     Card (
         modifier = modifier.fillMaxWidth(),
         colors = CardDefaults.cardColors(containerColor = Color.White),
         elevation = CardDefaults.elevatedCardElevation(defaultElevation = 4.dp),
-        shape = RoundedCornerShape(8.dp)
+        shape = RoundedCornerShape(8.dp),
     ) {
-        Row (
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Color.White)
-                .padding(8.dp)
-            ){
-            Image(
-                painter = painterResource(R.drawable.profileimage),
-                contentDescription = "Studio Profile Image",
-                modifier = Modifier.align(Alignment.CenterVertically)
-                    .size(52.dp)
-                    .clip(RoundedCornerShape(20.dp)),
-                contentScale = ContentScale.Crop
-            )
-            Column(modifier = Modifier
+        Column(modifier = Modifier.padding(16.dp)) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.profileimage),
+                    contentDescription = "Studio Profile Image",
+                    modifier = Modifier
                         .align(Alignment.CenterVertically)
-                        .padding(8.dp)){
-                Text(
-                    text = "두콩스튜디오",
-                    style = MaterialTheme.typography.bodyLarge,
+                        .size(52.dp)
+                        .clip(RoundedCornerShape(20.dp)),
+                    contentScale = ContentScale.Crop
                 )
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
+                Column(
+                    modifier = Modifier
+                        .align(Alignment.CenterVertically)
+                        .padding(8.dp)
                 ) {
-                    Icon(
-                        imageVector = Icons.Default.DateRange,
-                        contentDescription = "Calender",
-                        tint = Color.Gray
-                    )
                     Text(
-                        text = "2024-12-08 13:00",
-                        style = MaterialTheme.typography.bodySmall,
+                        text = "두콩스튜디오",
+                        style = MaterialTheme.typography.bodyLarge,
                     )
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.DateRange,
+                            contentDescription = "Calender",
+                            tint = Color.Gray
+                        )
+                        Text(
+                            text = "2024-12-08 13:00",
+                            style = MaterialTheme.typography.bodySmall,
+                        )
+                    }
                 }
-            }
-            Spacer(modifier = Modifier.weight(1f))
+                Spacer(modifier = Modifier.weight(1f))
 
                 SuggestionChip(
                     onClick = {},
                     enabled = false,
-                    label = { Text("예약대기") },
+                    label = {
+                        Text(
+                            text = "예약대기",
+                            style = MaterialTheme.typography.labelSmall
+                        )
+                    },
                     modifier = Modifier.align(Alignment.Top),
-                    shape = RoundedCornerShape(8.dp)
+                    shape = RoundedCornerShape(6.dp),
+                    colors = SuggestionChipDefaults.suggestionChipColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        disabledContainerColor = MaterialTheme.colorScheme.primaryContainer,
+                        labelColor = MaterialTheme.colorScheme.onPrimary,
+                        disabledLabelColor = MaterialTheme.colorScheme.onPrimary
+                    ),
+                    border = BorderStroke(1.dp, MaterialTheme.colorScheme.primaryContainer),
                 )
+            }
+
+            if (showButton) {
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Button(
+                        onClick = {},
+                        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.background),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(0.5f),
+                        shape = RoundedCornerShape(8.dp)
+                    ) {
+                        Text(
+                            text = "스튜디오 홈",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    Button(
+                        onClick = {},
+                        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.background),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(0.5f),
+                        shape = RoundedCornerShape(8.dp)
+                    ) {
+                        Text(
+                            text = "예약일정 변경",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
+            }
         }
     }
 }
@@ -89,8 +158,11 @@ fun bookingScheduleItemComponent(modifier: Modifier = Modifier){
 @Preview
 @Composable
 fun bookingScheduleItemPreview(){
-    Surface {
-        bookingScheduleItemComponent()
-    }
 
+    ToucheeseAppTheme {
+        Surface {
+
+            BookingScheduleItemComponent()
+        }
+    }
 }
