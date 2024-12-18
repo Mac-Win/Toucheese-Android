86ffc975ec98b75adf05ff0563dc6b8b18b1214f
TedMoon99 <mtj1015@naver.com>
2024-12-18 21:16:55 +0900

    [UI] 아이콘 적용

--- app/src/main/java/com/toucheese/app/ui/screens/tab_Home/OrderPayScreen.kt
+++ app/src/main/java/com/toucheese/app/ui/screens/tab_Home/OrderPayScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -73,6 +75,7 @@ fun OrderPayScreen(
         topBar = {
             TopAppBarComponent(
                 title = "주문/결제",
+                leadingIcon = Icons.AutoMirrored.Default.ArrowBack,
                 showLeadingIcon = true,
                 showTrailingIcon = false,
                 onClickLeadingIcon = onBackClick,
