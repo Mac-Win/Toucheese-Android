516c14f71c4006a06f8644b5e3c7e0bd6c0f4451
songjuseok <realsongzi93@gmail.com>
2024-12-23 15:52:05 +0900

    [Feat] KakaoAuthCallbackResponse.kt 데이터 수정

--- app/src/main/java/com/toucheese/app/data/model/SocialLogin/Kakao/KakaoAuthCallbackResponse.kt
+++ app/src/main/java/com/toucheese/app/data/model/SocialLogin/Kakao/KakaoAuthCallbackResponse.kt
@@ -3,8 +3,14 @@ package com.toucheese.app.data.model.SocialLogin.Kakao
 import com.google.gson.annotations.SerializedName
 
 data class KakaoAuthCallbackResponse(
+    @SerializedName("deviceId")
+    val deviceId: String,
     @SerializedName("isFirstLogin")
     val isFirstLogin: Boolean,
+    @SerializedName("memberId")
+    val memberId: Int,
     @SerializedName("nickname")
-    val nickname: String
+    val nickname: String,
+    @SerializedName("refreshToken")
+    val refreshToken: String
 )
