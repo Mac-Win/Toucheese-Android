779ec839320284c812e802ad30bc504a276c816e
TedMoon99 <mtj1015@naver.com>
2024-12-23 13:55:08 +0900

    [UI] 정보 아이템 UI 수정

--- app/src/main/java/com/toucheese/app/ui/components/listitem/InfoListitemComponent_NoChip.kt
+++ app/src/main/java/com/toucheese/app/ui/components/listitem/InfoListitemComponent_NoChip.kt
@@ -1,6 +1,8 @@
 package com.toucheese.app.ui.components.listitem
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CardDefaults
@@ -8,6 +10,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,6 +32,7 @@ fun InfoListItemComponentNoChip(
         ),
     ) {
         Column(
+            verticalArrangement = Arrangement.spacedBy(12.dp),
             modifier = modifier
         ) {
 
@@ -37,14 +41,22 @@ fun InfoListItemComponentNoChip(
                 style = MaterialTheme.typography.bodyLarge
             )
             Text(
-                text = "작성일: $createDate",
-                style = MaterialTheme.typography.bodyMedium
-            )
-            Text(
                 text = content,
                 style = MaterialTheme.typography.bodyMedium,
                 color = Color(0xFF434343),
             )
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Text(
+                    text = "터치즈 담당자",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Text(
+                    text = " | 작성일: $createDate",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color(0xFF8C8C8C)
+                )
+            }
         }
     }
 }
@@ -56,7 +68,7 @@ private fun InfoListItemNoChipPreview() {
     ToucheeseAppTheme {
         InfoListItemComponentNoChip(
             title = "터치즈 담당자",
-            content = "안녕하세요 OOO님.\n 문의내용 답변이 들어갑니다. 감사합니다.",
+            content = "안녕하세요 OOO님.\n문의내용 답변이 들어갑니다. 감사합니다.",
             createDate = "2024-11-04",
             modifier = Modifier.fillMaxWidth().padding(
                 start = 12.dp,
