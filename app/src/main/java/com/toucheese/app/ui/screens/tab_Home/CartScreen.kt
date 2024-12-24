d35d60a8e88ce4ec4e0df621278f86182b129c60
TedMoon99 <mtj1015@naver.com>
2024-12-24 22:35:16 +0900

    [Fix] 장바구니 카트 아이템 선택 및 전체선택 로직 수정 & 예약 총 가격 계산 수정

--- app/src/main/java/com/toucheese/app/ui/screens/tab_Home/CartScreen.kt
+++ app/src/main/java/com/toucheese/app/ui/screens/tab_Home/CartScreen.kt
@@ -2,13 +2,16 @@ package com.toucheese.app.ui.screens.tab_Home
 
 import android.util.Log
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -21,6 +24,8 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SuggestionChip
+import androidx.compose.material3.SuggestionChipDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
@@ -42,6 +47,7 @@ import com.toucheese.app.data.model.home.carts_list.CartListResponseItem
 import com.toucheese.app.data.token_manager.TokenManager
 import com.toucheese.app.ui.components.CartItemComponent
 import com.toucheese.app.ui.components.ChangeOptionBottomSheetComponent
+import com.toucheese.app.ui.components.SquareRadioButton
 import com.toucheese.app.ui.components.topbar.TopAppBarComponent
 import com.toucheese.app.ui.viewmodel.HomeViewModel
 import kotlinx.coroutines.launch
@@ -59,7 +65,7 @@ fun CartScreen(
 
     val cartItems by viewModel.cartItems.collectAsState() // ViewModel에서 상태 관찰
     var isBottomSheetVisible by remember { mutableStateOf(false) }
-    var selectedItem by remember { mutableStateOf<com.toucheese.app.data.model.home.carts_list.CartListResponseItem?>(null) }
+    var selectedItem by remember { mutableStateOf<CartListResponseItem?>(null) }
     // 장바구니 내역이 있는지 확인
     val isCartItemsExists = cartItems.isNotEmpty()
     // BottomSheetModal 상태 관리
@@ -77,9 +83,6 @@ fun CartScreen(
             bottomSheetState.expand()
         }
     }
-
-    // 총합 계산
-    val totalAmount = "₩${cartItems.sumOf { it.totalPrice }}"
 
     // 장바구니 아이템 삭제 로직
     fun onDeleteCartItem(cartItem: CartListResponseItem) {
@@ -107,12 +110,23 @@ fun CartScreen(
 
     // `selectedItem`이 변경될 때마다 `selectedOptionIds` 업데이트
     LaunchedEffect(selectedItem) {
-        selectedOptionIds = selectedItem?.selectAddOptions?.map { it.selectOptionId }?.toSet() ?: setOf()
+        selectedOptionIds =
+            selectedItem?.selectAddOptions?.map { it.selectOptionId }?.toSet() ?: setOf()
         Log.d("CartScreen", "SelectedOptionIds initialized: $selectedOptionIds")
     }
 
     // 선택한 장바구니 아이템 데이터
-    var selectedCartItem by remember { mutableStateOf<Set<Int>>( emptySet()) }
+    var selectedCartItem by remember { mutableStateOf<Set<Int>>(emptySet()) }
+    // 전체 선택 여부
+    val isAllItemClicked = selectedCartItem.size == cartItems.size
+    // 선택된 아이템 총합 계산
+    val cartTotalPrice = cartItems.filter { cartItem ->
+        // 불러온 장바구니 데이터 중에서 선택된 Id에 포함되는 데이터만 추출
+        selectedCartItem.contains(cartItem.cartId)
+    }.sumOf { cartItem ->
+        // 정제된 장바구니 데이터 중 총 가격의 총합을 구함
+        cartItem.totalPrice
+    }
 
     Scaffold(
         modifier = Modifier.safeDrawingPadding(),
@@ -123,7 +137,7 @@ fun CartScreen(
                 showLeadingIcon = true,
                 showTrailingIcon = false,
                 onClickLeadingIcon = onBackClick,
-                onClickTrailingIcon = { /* 필요시 구현 */}
+                onClickTrailingIcon = { /* 필요시 구현 */ }
             )
         },
         bottomBar = {
@@ -141,7 +155,12 @@ fun CartScreen(
                         contentColor = Color(0xFF1F1F1F),
                         disabledContentColor = Color(0xFFA2A2A2),
                     ),
-                    border = BorderStroke(1.dp, if (isCartItemsExists) MaterialTheme.colorScheme.primary else Color(0xFFD9D9D9)),
+                    border = BorderStroke(
+                        1.dp,
+                        if (isCartItemsExists) MaterialTheme.colorScheme.primary else Color(
+                            0xFFD9D9D9
+                        )
+                    ),
                     onClick = {
                         val cartIds = cartItems.map { it.cartId }
                         onCheckoutClick(cartIds)
@@ -151,7 +170,7 @@ fun CartScreen(
                         .padding(8.dp),
                 ) {
                     Text(
-                        text = "예약하기 (총 ${totalAmount}원)",
+                        text = "예약하기 (총 ${cartTotalPrice}원)",
                         fontSize = 16.sp,
                         fontWeight = FontWeight.Bold
                     )
@@ -182,6 +201,66 @@ fun CartScreen(
                         }
                     }
                 } else {
+                    item {
+                        // 전체 선택 및 선택 상품 삭제
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Start,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(Color.White)
+                                .padding(horizontal = 16.dp, vertical = 8.dp)
+                        ) {
+                            // 선택 버튼
+                            SquareRadioButton(
+                                selected = selectedCartItem.size == cartItems.size,
+                                selectedColor = MaterialTheme.colorScheme.primary,
+                                unselectedColor = Color.Transparent,
+                                borderColor = if (isAllItemClicked) Color.Black else Color(0xFFBFBFBF),
+                                onClick = {
+                                    if (isAllItemClicked) {
+                                        // 전체 선택을 해제하는 경우
+                                        selectedCartItem = emptySet()
+                                    } else {
+                                        // 전체 선택을 설정하는 경우
+                                        selectedCartItem = selectedCartItem.toMutableSet().apply {
+                                            clear() // 초기화
+                                            cartItems.forEach { cartItem -> add(cartItem.cartId) }
+                                        }
+                                    }
+                                },
+                            )
+
+                            Spacer(modifier = Modifier.width(8.dp))
+
+                            // 전체 선택 버튼
+                            Text(
+                                text = "전체 선택",
+                                style = MaterialTheme.typography.bodyLarge
+                            )
+
+                            Spacer(modifier = Modifier.weight(1f))
+
+                            // 선택 상품 삭제
+                            SuggestionChip(
+                                shape = RoundedCornerShape(8.dp),
+                                label = {
+                                    Text(
+                                        text = "선택 상품 삭제",
+                                        style = MaterialTheme.typography.bodyLarge
+                                    )
+                                },
+                                border = BorderStroke(1.dp, Color(0xFFF0F0F0)),
+                                colors = SuggestionChipDefaults.suggestionChipColors(
+                                    containerColor = Color(0xFFFAFAFA),
+                                    labelColor = Color(0xFF1F1F1F)
+                                ),
+                                onClick = {
+//                                    viewModel.deleteCartItem(token, c)
+                                }
+                            )
+                        }
+                    }
                     items(
                         items = cartItems,
                     ) { cartItem ->
@@ -272,20 +351,21 @@ fun CartScreen(
                     },
                     onOptionClicked = { optionId ->
                         // 옵션 선택/해제 로직
-                        val updatedOptions = if (item.selectAddOptions.any { it.selectOptionId == optionId }) {
-                            item.selectAddOptions.filterNot { it.selectOptionId == optionId }
-                        } else {
-                            val selectedOption = item.addOptions.find { it.id == optionId }
-                            if (selectedOption != null) {
-                                item.selectAddOptions + com.toucheese.app.data.model.home.carts_list.SelectAddOption(
-                                    selectOptionId = selectedOption.id,
-                                    selectOptionName = selectedOption.name,
-                                    selectOptionPrice = selectedOption.price
-                                )
+                        val updatedOptions =
+                            if (item.selectAddOptions.any { it.selectOptionId == optionId }) {
+                                item.selectAddOptions.filterNot { it.selectOptionId == optionId }
                             } else {
-                                item.selectAddOptions
+                                val selectedOption = item.addOptions.find { it.id == optionId }
+                                if (selectedOption != null) {
+                                    item.selectAddOptions + com.toucheese.app.data.model.home.carts_list.SelectAddOption(
+                                        selectOptionId = selectedOption.id,
+                                        selectOptionName = selectedOption.name,
+                                        selectOptionPrice = selectedOption.price
+                                    )
+                                } else {
+                                    item.selectAddOptions
+                                }
                             }
-                        }
                         val updatedOptionIds = updatedOptions.map { it.selectOptionId }.toSet()
                         val changedCartItem =
                             com.toucheese.app.data.model.home.carts_optionChange.ChangedCartItem(
@@ -332,7 +412,7 @@ fun CartScreen(
                         Log.d("CartScreen", "SelectedOptionIds after change: $selectedOptionIds")
                     },
 
-                )
+                    )
             }
         }
     }
