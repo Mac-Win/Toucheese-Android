e9a78ecf9b6ed1bf7be092e0f5cde6562a1c0f4d
songjuseok <realsongzi93@gmail.com>
2024-12-26 18:07:58 +0900

    [UI] TabBarComponent.kt 피그마 디자인에 맞게 탭 바 수정

--- app/src/main/java/com/toucheese/app/ui/components/TabBarComponent.kt
+++ app/src/main/java/com/toucheese/app/ui/components/TabBarComponent.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -23,29 +24,31 @@ fun TabBarComponent(
 ) {
     TabRow(
         selectedTabIndex = selectedTabIndex,
-        containerColor = Color(0xFFFFF2CC),
+        containerColor = Color.White, // TabRow 배경을 흰색으로 설정
         indicator = { tabPositions ->
             TabRowDefaults.Indicator(
-                Modifier.tabIndicatorOffset(tabPositions[selectedTabIndex]),
-                color = Color(0xFFFFC000)
+                Modifier.tabIndicatorOffset(tabPositions[selectedTabIndex])
+                        .padding(horizontal = 8.dp),
+                color = MaterialTheme.colorScheme.primary // Indicator 색상을 primary 색상으로 설정
             )
         },
-        modifier = Modifier.clip(RoundedCornerShape(8.dp))
+        modifier = Modifier
+            .background(Color.White)
+            .padding(vertical = 8.dp)
     ) {
         tabTitles.forEachIndexed { index, title ->
             Tab(
                 selected = selectedTabIndex == index,
                 onClick = { onTabSelected(index) },
                 modifier = Modifier
-                    .background(
-                        if (selectedTabIndex == index) Color(0xFFFFC000) else Color(0xFFFFF2CC)
-                    )
+                    .background(Color.White)
             ) {
                 Text(
                     text = title,
-                    style = MaterialTheme.typography.bodyMedium,
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontWeight = FontWeight.Bold,
                     modifier = Modifier.padding(8.dp),
-                    color = Color.Black
+                    color = if (selectedTabIndex == index) Color.Black else Color.Gray // 선택 여부에 따라 텍스트 색상 변경
                 )
             }
         }
