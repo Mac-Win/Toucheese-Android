a56d8ad672ec191e0abaa6e9d83f2f2090a30d9e
songjuseok <realsongzi93@gmail.com>
2024-12-26 18:05:51 +0900

    [UI] ProductListComponent.kt 헤더 텍스트 추가

--- app/src/main/java/com/toucheese/app/ui/components/ProductListComponent.kt
+++ app/src/main/java/com/toucheese/app/ui/components/ProductListComponent.kt
@@ -1,30 +1,54 @@
 package com.toucheese.app.ui.components
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.toucheese.app.data.model.home.studio_detail.Product
 
 @Composable
-fun ProductList(products: List<com.toucheese.app.data.model.home.studio_detail.Product>, modifier: Modifier = Modifier, onProductClicked: (Int) -> Unit) {
-    Column(modifier = modifier) { // LazyColumn 대신 Column 사용
-        products.forEach { product ->
-            ProductItem(
-                product = product,
-                modifier = Modifier
-                    .fillMaxWidth() // 가로는 전체 크기
-                    .wrapContentHeight() // 세로는 콘텐츠 크기에 맞게 설정
-                    .padding(16.dp)
-                    .clickable {
-                        // 상품 클릭 시 상품번호 전달
-                        onProductClicked(product.id)
-                    }
-            )
+fun ProductList(
+    products: List<Product>,
+    modifier: Modifier = Modifier,
+    onProductClicked: (Int) -> Unit
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp) // 전체 리스트의 패딩
+    ) {
+        // 헤더 텍스트 추가
+        Text(
+            text = "촬영 상품",
+            style = MaterialTheme.typography.titleMedium.copy(
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onPrimary
+            ),
+            modifier = Modifier
+                .padding(bottom = 8.dp) // 헤더와 상품 목록 사이 간격
+        )
+
+        // 상품 목록 표시
+        Column {
+            products.forEach { product ->
+                ProductItem(
+                    product = product,
+                    modifier = Modifier
+                        .fillMaxWidth() // 가로는 전체 크기
+                        .wrapContentHeight() // 세로는 콘텐츠 크기에 맞게 설정
+                        .clickable {
+                            // 상품 클릭 시 상품번호 전달
+                            onProductClicked(product.id)
+                        }
+                        .padding(vertical = 8.dp) // 각 상품 간 세로 간격
+                )
+            }
         }
     }
 }
