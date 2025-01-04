e4278372d1ad263ceaa0459e5f689ebcac5acfd0
TedMoon99 <mtj1015@naver.com>
2025-01-04 20:07:42 +0900

    [Gradle] 설정 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.toucheese.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
+        versionCode = 11
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
